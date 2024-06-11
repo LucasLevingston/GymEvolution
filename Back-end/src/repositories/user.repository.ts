c5136b47280eb5878426684ce9931cc4f9d888ac
Lucas Henrique <lucaolevingston@outlook.com>
2024-06-11 20:53:04 -0300

    novo alterarDado do treino

--- Back-end/src/repositories/user.repository.ts
+++ Back-end/src/repositories/user.repository.ts
@@ -1,145 +1,178 @@
 import { Historico, Peso } from './../interfaces/user.interface';
-import { prisma } from "../database/prisma.client";
-import { User, UserCreate, UserRepository } from "../interfaces/user.interface";
+import { prisma } from '../database/prisma.client';
+import { User, UserCreate, UserRepository } from '../interfaces/user.interface';
 import { SemanaDeTreino, SemanaDeTreinoCreate } from '../interfaces/treino.interface';
 
 class UserRepositoryPrisma implements UserRepository {
-   async create(data: UserCreate): Promise<UserCreate> {
-      const result = await prisma.user.create({
-         data: {
-            email: data.email,
-            senha: data.senha,
-            nascimento: "0"
-         }
+  async create(data: UserCreate): Promise<UserCreate> {
+    const result = await prisma.user.create({
+      data: {
+        email: data.email,
+        senha: data.senha,
+        nascimento: '0',
+      },
+    });
+    return result;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const result = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+    return result || null;
+  }
+  async login(email: string, senha: string): Promise<User | null> {
+    const user = await this.findByEmail(email);
+    if (user) {
+      return user.senha === senha ? user : null;
+    }
+    return null;
+  }
+  async getUser(email: string): Promise<User | null> {
+    const user = await this.findByEmail(email);
+    return user;
+  }
+  // async adicionarAoHistorico(email: string, field: string, novoValor: string | object) {
+  //    const user = await this.findByEmail(email);
+  //    try {
+  //       if (!user) {
+  //          throw new Error("Usuário não encontrado");
+  //       }
+  //       await prisma.user.update({
+  //          where: { email },
+  //          data: {
+  //             historico: {
+  //                create: {
+  //                   id: user.id,
+  //                   ocorrido: `O campo '${field} foi alterado para '${novoValor}'`,
+  //                   data: Date(),
+  //                }
+  //             }
+  //          }
+  //       }
+  //       )
+  //    } catch (error) {
+  //       throw new Error("Erro ao cadastrar no historico")
+  //    }
+  // }
+  //  async alterarDado(email: string, field: string, novoValor: string | object | Peso | SemanaDeTreinoCreate): Promise<{ field: string, novoValor: string | object } | null> {
+  //     const user = await this.findByEmail(email);
+  //     if (!user || !field || !novoValor) {
+  //        throw new Error("Usuário não encontrado");
+  //     }
+
+  //     if (typeof novoValor === 'string') {
+  //        await prisma.user.update({
+  //           where: { email },
+  //           data: { [field]: novoValor },
+  //        })
+  //        await prisma.historico.create({
+  //           data: {
+  //              userId: user.id,
+  //              ocorrido: `O campo '${field}' foi alterado para '${novoValor}'`,
+  //              data: new Date().toISOString(),
+  //           }
+  //        });
+  //        return { field, novoValor }
+  //     }
+
+  //     else if (field === 'peso' && typeof novoValor === 'object') {
+  //        const updatedUser = await prisma.user.update({
+  //           where: { email },
+  //           data: {
+  //              pesosAntigos: {
+  //                 create: {
+  //                    peso: (novoValor as Peso).peso,
+  //                    data: (novoValor as Peso).data,
+  //                    bf: (novoValor as Peso).bf,
+  //                 }
+  //              }
+  //           },
+  //           include: {
+  //              pesosAntigos: true
+  //           }
+  //        });
+  //        await prisma.historico.create({
+  //           data: {
+  //              userId: user.id,
+  //              ocorrido: `O '${field}' foi alterado para '${novoValor}'`,
+  //              data: new Date().toISOString(),
+  //           }
+  //        });
+  //        await prisma.user.update({
+  //           where: { email },
+  //           data: {
+  //              pesoAtual: updatedUser.pesosAntigos[updatedUser.pesosAntigos.length - 1].peso
+  //           },
+  //        });
+
+  //     }
+  //     if (field === "semanaDeTreino" && typeof novoValor === 'object') {
+  //        let novaSemana: SemanaDeTreino;
+
+  //        if ('treino' in novoValor && Array.isArray(novoValor.treino)) {
+  //           novaSemana = novoValor;
+  //        } else {
+  //           throw new Error("O tipo de dado fornecido não é válido para uma nova semana de treino");
+  //        }
+  //        const usuario = await prisma.user.findUnique({
+  //           where: { email },
+  //           include: { SemanasDeTreino: true }
+  //        });
+
+  //        if (!usuario) {
+  //           throw new Error("Usuário não encontrado");
+  //        }
+
+  //        const novasSemanas = [...usuario.SemanasDeTreino, novaSemana];
+  //        await prisma.user.update({
+  //           where: { email },
+  //           data: {
+  //              SemanasDeTreino: {
+  //                 create: novaSemana
+  //              }
+  //           }
+  //        });
+  //     }
+  //     return { field, novoValor };
+  //  }
+  async alterarDado(updatedData: User): Promise<User | null> {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: updatedData.email,
+        },
       });
+
+      if (!user) {
+        throw new Error('User not found');
+      }
+
+      // Mapeie a array de histórico atualizada para o formato esperado pelo Prisma
+      const historicoUpdateInputs = updatedData.historico.map((historicoItem) => ({
+        where: { id: historicoItem.id }, // Você precisa fornecer uma chave única para cada item de histórico
+        data: { ...historicoItem }, // Espalha as propriedades do item de histórico
+      }));
+
+      const result = await prisma.user.update({
+        where: {
+          email: updatedData.email,
+        },
+        data: {
+          ...updatedData,
+          historico: {
+            // Use 'updateMany' para atualizar vários registros de histórico
+            updateMany: historicoUpdateInputs,
+          },
+        },
+      });
+
       return result;
-   }
-
-   async findByEmail(email: string): Promise<User | null> {
-      const result = await prisma.user.findFirst({
-         where: {
-            email
-         }
-      });
-      return result || null;
-   }
-   async login(email: string, senha: string): Promise<User | null> {
-      const user = await this.findByEmail(email)
-      if (user) {
-         return user.senha === senha ? user : null
-      }
-      return null
-   }
-   async getUser(email: string): Promise<User | null> {
-      const user = await this.findByEmail(email)
-      return user
-   }
-   // async adicionarAoHistorico(email: string, field: string, novoValor: string | object) {
-   //    const user = await this.findByEmail(email);
-   //    try {
-   //       if (!user) {
-   //          throw new Error("Usuário não encontrado");
-   //       }
-   //       await prisma.user.update({
-   //          where: { email },
-   //          data: {
-   //             historico: {
-   //                create: {
-   //                   id: user.id,
-   //                   ocorrido: `O campo '${field} foi alterado para '${novoValor}'`,
-   //                   data: Date(),
-   //                }
-   //             }
-   //          }
-   //       }
-   //       )
-   //    } catch (error) {
-   //       throw new Error("Erro ao cadastrar no historico")
-   //    }
-   // }
-   async alterarDado(email: string, field: string, novoValor: string | object | Peso | SemanaDeTreinoCreate): Promise<{ field: string, novoValor: string | object } | null> {
-      const user = await this.findByEmail(email);
-      if (!user || !field || !novoValor) {
-         throw new Error("Usuário não encontrado");
-      }
-
-      if (typeof novoValor === 'string') {
-         await prisma.user.update({
-            where: { email },
-            data: { [field]: novoValor },
-         })
-         await prisma.historico.create({
-            data: {
-               userId: user.id,
-               ocorrido: `O campo '${field}' foi alterado para '${novoValor}'`,
-               data: new Date().toISOString(),
-            }
-         });
-         return { field, novoValor }
-      }
-
-      else if (field === 'peso' && typeof novoValor === 'object') {
-         const updatedUser = await prisma.user.update({
-            where: { email },
-            data: {
-               pesosAntigos: {
-                  create: {
-                     peso: (novoValor as Peso).peso,
-                     data: (novoValor as Peso).data,
-                     bf: (novoValor as Peso).bf,
-                  }
-               }
-            },
-            include: {
-               pesosAntigos: true
-            }
-         });
-         await prisma.historico.create({
-            data: {
-               userId: user.id,
-               ocorrido: `O '${field}' foi alterado para '${novoValor}'`,
-               data: new Date().toISOString(),
-            }
-         });
-         await prisma.user.update({
-            where: { email },
-            data: {
-               pesoAtual: updatedUser.pesosAntigos[updatedUser.pesosAntigos.length - 1].peso
-            },
-         });
-
-      }
-      if (field === "semanaDeTreino" && typeof novoValor === 'object') {
-         let novaSemana: SemanaDeTreino;
-
-         if ('treino' in novoValor && Array.isArray(novoValor.treino)) {
-            novaSemana = novoValor;
-         } else {
-            throw new Error("O tipo de dado fornecido não é válido para uma nova semana de treino");
-         }
-         const usuario = await prisma.user.findUnique({
-            where: { email },
-            include: { SemanasDeTreino: true }
-         });
-
-         if (!usuario) {
-            throw new Error("Usuário não encontrado");
-         }
-
-         const novasSemanas = [...usuario.SemanasDeTreino, novaSemana];
-         await prisma.user.update({
-            where: { email },
-            data: {
-               SemanasDeTreino: {
-                  create: novaSemana
-               }
-            }
-         });
-      }
-      return { field, novoValor };
-   }
-
-
-
+    } catch (error) {
+      throw new Error('Fail');
+    }
+  }
 }
 export { UserRepositoryPrisma };
--- Front-end/src/Paginas/NovoTreino.tsx
+++ Front-end/src/Paginas/NovoTreino.tsx
@@ -4,218 +4,255 @@ import { Input } from '@/components/ui/input';
 import useUser from '@/hooks/user-hooks';
 import { SemanaDeTreinoCreate } from '@/types/treinoType';
 import { UserType } from '@/types/userType';
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import { useEffect, useState, ChangeEvent } from 'react';
 
 export default function NovoTreino() {
-   const { getUser } = useUser();
-   const [user, setUser] = useState<UserType | null>(null);
-   const [semanaDeTreino, setSemanaDeTreino] = useState<SemanaDeTreinoCreate>({
-      informacoes: '',
-      feito: false,
-      treino: [{
-         grupo: '',
-         diaDaSemana: '',
-         feito: false,
-         observacoes: '',
-         exercicios: [{
-            nome: '',
-            variacao: '',
-            repeticoes: '',
-            quantidadeDeSeries: '',
-            feito: false,
-            resultado: [{
-               serieIndex: '',
-               repeticoes: '',
-               carga: ''
-            }]
-         }]
-      }]
-   });
+	const { getUser } = useUser();
+	const [user, setUser] = useState<UserType | null>(null);
+	const [semanaDeTreino, setSemanaDeTreino] = useState<SemanaDeTreinoCreate>({
+		informacoes: '',
+		feito: false,
+		treino: [
+			{
+				grupo: '',
+				diaDaSemana: '',
+				feito: false,
+				observacoes: '',
+				exercicios: [
+					{
+						nome: '',
+						variacao: '',
+						repeticoes: '',
+						quantidadeDeSeries: '',
+						feito: false,
+						resultado: [
+							{
+								serieIndex: '',
+								repeticoes: '',
+								carga: '',
+							},
+						],
+					},
+				],
+			},
+		],
+	});
 
-   useEffect(() => {
-      const fetchUser = async () => {
-         const fetchedUser = await getUser();
-         setUser(fetchedUser);
-      };
+	useEffect(() => {
+		const fetchUser = async () => {
+			const fetchedUser = await getUser();
+			setUser(fetchedUser);
+		};
 
-      fetchUser();
-   }, [getUser]);
+		fetchUser();
+	}, [getUser]);
 
-   const handleInputChange =
-      (e: ChangeEvent<HTMLInputElement>, treinoIndex: number, exercicioIndex?: number, resultadoIndex?: number) => {
-         const { name, value } = e.target;
-         setSemanaDeTreino((prevTreino) => {
-            const updatedTreino = { ...prevTreino };
-            if (name in updatedTreino) {
-               (updatedTreino as any)[name] = value;
-            } else if (exercicioIndex !== undefined && resultadoIndex !== undefined) {
-               updatedTreino.treino[treinoIndex].exercicios[exercicioIndex].resultado[resultadoIndex][name] = value;
-            } else if (exercicioIndex !== undefined) {
-               updatedTreino.treino[treinoIndex].exercicios[exercicioIndex][name] = value;
-            } else {
-               updatedTreino.treino[treinoIndex][name] = value;
-            }
-            return updatedTreino;
-         });
-      };
+	const handleInputChange = (
+		e: ChangeEvent<HTMLInputElement>,
+		treinoIndex: number,
+		exercicioIndex?: number,
+		resultadoIndex?: number
+	) => {
+		const { name, value } = e.target;
+		setSemanaDeTreino((prevTreino) => {
+			const updatedTreino = { ...prevTreino };
+			if (name in updatedTreino) {
+				(updatedTreino as any)[name] = value;
+			} else if (exercicioIndex !== undefined && resultadoIndex !== undefined) {
+				updatedTreino.treino[treinoIndex].exercicios[exercicioIndex].resultado[
+					resultadoIndex
+				][name] = value;
+			} else if (exercicioIndex !== undefined) {
+				updatedTreino.treino[treinoIndex].exercicios[exercicioIndex][name] =
+					value;
+			} else {
+				updatedTreino.treino[treinoIndex][name] = value;
+			}
+			return updatedTreino;
+		});
+	};
 
-   const handleAddExercicio = (treinoIndex: number) => {
-      setSemanaDeTreino((diasDeTreino) => {
-         const novaSemanaDeTreino = {
-            ...diasDeTreino,
-            treino: diasDeTreino.treino.map((treino, index) => {
-               if (index === treinoIndex) {
-                  return {
-                     ...treino,
-                     exercicios: [
-                        ...treino.exercicios,
-                        {
-                           nome: '',
-                           variacao: '',
-                           repeticoes: '',
-                           quantidadeDeSeries: '',
-                           feito: false,
-                           resultado: [
-                              {
-                                 serieIndex: '',
-                                 repeticoes: '',
-                                 carga: ''
-                              }
-                           ]
-                        }
-                     ]
-                  };
-               }
-               return treino;
-            })
-         };
-         return novaSemanaDeTreino;
-      });
-   };
+	const handleAddExercicio = (treinoIndex: number) => {
+		setSemanaDeTreino((diasDeTreino) => {
+			const novaSemanaDeTreino = {
+				...diasDeTreino,
+				treino: diasDeTreino.treino.map((treino, index) => {
+					if (index === treinoIndex) {
+						return {
+							...treino,
+							exercicios: [
+								...treino.exercicios,
+								{
+									nome: '',
+									variacao: '',
+									repeticoes: '',
+									quantidadeDeSeries: '',
+									feito: false,
+									resultado: [
+										{
+											serieIndex: '',
+											repeticoes: '',
+											carga: '',
+										},
+									],
+								},
+							],
+						};
+					}
+					return treino;
+				}),
+			};
+			return novaSemanaDeTreino;
+		});
+	};
 
-   const handleAddTreino = () => {
-      setSemanaDeTreino((prevTreino) => {
-         const novoTreino = {
-            grupo: '',
-            diaDaSemana: '',
-            feito: false,
-            observacoes: '',
-            exercicios: [{
-               nome: '',
-               variacao: '',
-               repeticoes: '',
-               quantidadeDeSeries: '',
-               feito: false,
-               resultado: [{
-                  serieIndex: '',
-                  repeticoes: '',
-                  carga: ''
-               }]
-            }]
-         };
-         return {
-            ...prevTreino,
-            treino: [...prevTreino.treino, novoTreino]
-         };
-      });
-   };
+	const handleAddTreino = () => {
+		setSemanaDeTreino((prevTreino) => {
+			const novoTreino = {
+				grupo: '',
+				diaDaSemana: '',
+				feito: false,
+				observacoes: '',
+				exercicios: [
+					{
+						nome: '',
+						variacao: '',
+						repeticoes: '',
+						quantidadeDeSeries: '',
+						feito: false,
+						resultado: [
+							{
+								serieIndex: '',
+								repeticoes: '',
+								carga: '',
+							},
+						],
+					},
+				],
+			};
+			return {
+				...prevTreino,
+				treino: [...prevTreino.treino, novoTreino],
+			};
+		});
+	};
 
-   const handleSubmit = async () => {
-      console.log('Treino data:', semanaDeTreino);
-   };
+	const handleSubmit = async () => {
+		console.log('Treino data:', semanaDeTreino);
+	};
 
-   const numeroDaSemana = () => {
-      if (user?.SemanasDeTreino === undefined) return 0
-      return user?.SemanasDeTreino?.length + 1
-   }
+	const numeroDaSemana = () => {
+		if (user?.SemanasDeTreino === undefined) return 0;
+		return user?.SemanasDeTreino?.length + 1;
+	};
 
-   return (
-      <Container>
-         Semana: {numeroDaSemana()}
-         {user && (
-            <div className='w-full h-screen text-preto space-y-3'>
-               <Input
-                  value={semanaDeTreino.informacoes}
-                  name="informacoes"
-                  placeholder='Informações (opcional)'
-                  className='w-1/2'
-                  onChange={(e) => handleInputChange(e, 0)}
-               />
-               {semanaDeTreino.treino.map((treino, treinoIndex) => (
-                  <div className='bg-branco p-2' key={treinoIndex}>
-                     <div className='flex flex-wrap w-[100%] space-y-3 rounded'>
-                        Dia {treinoIndex + 1} ({treino.diaDaSemana})
-                        <Input
-                           value={treino.grupo}
-                           name="grupo"
-                           placeholder='Grupo muscular'
-                           onChange={(e) => handleInputChange(e, treinoIndex)}
-                        />
-                        <Input
-                           value={treino.diaDaSemana}
-                           name="diaDaSemana"
-                           placeholder='Dia da Semana'
-                           onChange={(e) => handleInputChange(e, treinoIndex)}
-                        />
-                        <div className='flex flex-wrap gap-3'>
-                           {treino.exercicios.map((exercicio, exercicioIndex) => (
-                              <div key={exercicioIndex} className='space-y-3 w-[400px] bg-cinzaEscuro rounded p-3'>
-                                 <p className='text-branco'>Exercício {exercicioIndex + 1}</p>
-                                 <div>
-                                    <p className='text-branco text-sm pb-1'>Nome do exercicio</p>
-                                    <Input
-                                       value={exercicio.nome}
-                                       name="nome"
-                                       placeholder='Nome do Exercício'
-                                       onChange={(e) => handleInputChange(e, treinoIndex, exercicioIndex)}
-                                    />
-                                 </div>
-                                 <div>
-                                    <p className='text-branco text-sm pb-1'>Repetições</p>
-                                    <Input
-                                       value={exercicio.repeticoes}
-                                       name="repeticoes"
-                                       placeholder='Repetições'
-                                       onChange={(e) => handleInputChange(e, treinoIndex, exercicioIndex)}
-                                    />
-                                 </div>
-                                 <div>
-                                    <p className='text-branco text-sm pb-1'>Quantidade de séries</p>
-                                    <Input
-                                       value={exercicio.quantidadeDeSeries}
-                                       name="quantidadeDeSeries"
-                                       placeholder='Quantidade de Séries'
-                                       onChange={(e) => handleInputChange(e, treinoIndex, exercicioIndex)}
-                                    />
-                                 </div>
-                                 <div>
-                                    <p className='text-branco text-sm pb-1'>Variação</p>
-                                    <Input
-                                       value={exercicio.variacao}
-                                       name="variacao"
-                                       placeholder='Variação (opcional)'
-                                       onChange={(e) => handleInputChange(e, treinoIndex, exercicioIndex)}
-                                    />
-                                 </div>
-                              </div>
-                           ))}
-                        </div>
-                     </div>
-                     <div className='text-right'>
-                        <Button onClick={() => handleAddExercicio(treinoIndex)} className='w-[200px]'>
-                           Adicionar Exercício
-                        </Button>
-                     </div>
-                  </div>
-               ))}
-               <Button onClick={handleAddTreino} className='w-full mt-4'>
-                  Adicionar Dia de Treino
-               </Button>
-               <Button onClick={handleSubmit} className='w-full mt-4'>
-                  Salvar Treino
-               </Button>
-            </div>
-         )}
-      </Container>
-   );
+	return (
+		<Container>
+			Semana: {numeroDaSemana()}
+			{user && (
+				<div className="h-screen w-full space-y-3 text-preto">
+					<Input
+						value={semanaDeTreino.informacoes}
+						name="informacoes"
+						placeholder="Informações (opcional)"
+						className="w-1/2"
+						onChange={(e) => handleInputChange(e, 0)}
+					/>
+					{semanaDeTreino.treino.map((treino, treinoIndex) => (
+						<div className="bg-branco p-2" key={treinoIndex}>
+							<div className="flex w-[100%] flex-wrap space-y-3 rounded">
+								Dia {treinoIndex + 1} ({treino.diaDaSemana})
+								<Input
+									value={treino.grupo}
+									name="grupo"
+									placeholder="Grupo muscular"
+									onChange={(e) => handleInputChange(e, treinoIndex)}
+								/>
+								<Input
+									value={treino.diaDaSemana}
+									name="diaDaSemana"
+									placeholder="Dia da Semana"
+									onChange={(e) => handleInputChange(e, treinoIndex)}
+								/>
+								<div className="flex flex-wrap gap-3">
+									{treino.exercicios.map((exercicio, exercicioIndex) => (
+										<div
+											key={exercicioIndex}
+											className="w-[400px] space-y-3 rounded bg-cinzaEscuro p-3"
+										>
+											<p className="text-branco">
+												Exercício {exercicioIndex + 1}
+											</p>
+											<div>
+												<p className="pb-1 text-sm text-branco">
+													Nome do exercicio
+												</p>
+												<Input
+													value={exercicio.nome}
+													name="nome"
+													placeholder="Nome do Exercício"
+													onChange={(e) =>
+														handleInputChange(e, treinoIndex, exercicioIndex)
+													}
+												/>
+											</div>
+											<div>
+												<p className="pb-1 text-sm text-branco">Repetições</p>
+												<Input
+													value={exercicio.repeticoes}
+													name="repeticoes"
+													placeholder="Repetições"
+													onChange={(e) =>
+														handleInputChange(e, treinoIndex, exercicioIndex)
+													}
+												/>
+											</div>
+											<div>
+												<p className="pb-1 text-sm text-branco">
+													Quantidade de séries
+												</p>
+												<Input
+													value={exercicio.quantidadeDeSeries}
+													name="quantidadeDeSeries"
+													placeholder="Quantidade de Séries"
+													onChange={(e) =>
+														handleInputChange(e, treinoIndex, exercicioIndex)
+													}
+												/>
+											</div>
+											<div>
+												<p className="pb-1 text-sm text-branco">Variação</p>
+												<Input
+													value={exercicio.variacao}
+													name="variacao"
+													placeholder="Variação (opcional)"
+													onChange={(e) =>
+														handleInputChange(e, treinoIndex, exercicioIndex)
+													}
+												/>
+											</div>
+										</div>
+									))}
+								</div>
+							</div>
+							<div className="text-right">
+								<Button
+									onClick={() => handleAddExercicio(treinoIndex)}
+									className="w-[200px]"
+								>
+									Adicionar Exercício
+								</Button>
+							</div>
+						</div>
+					))}
+					<Button onClick={handleAddTreino} className="mt-4 w-full">
+						Adicionar Dia de Treino
+					</Button>
+					<Button onClick={handleSubmit} className="mt-4 w-full">
+						Salvar Treino
+					</Button>
+				</div>
+			)}
+		</Container>
+	);
 }
